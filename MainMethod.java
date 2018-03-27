97d95deb3522b7f590d2072478bc61d5e67ee3c0
Emily Crawford <e.c.crawford@live.ca>
2018-03-27 13:26:44 -0400

    implemented stage 0.4

--- MainMethod.java
+++ MainMethod.java
@@ -4,15 +4,12 @@
 // This program demonstrates synchronizattion of processes that read and 
 // write a shared data structure.  Several readers can access the data 
 // simultaneously, or one writer can have exclusive access.
-//
-// In lab 3 you were given code that implemented the synchronization using
-// semaphores.  Here is the same algorithm coded to use Java monitors.
-
-// This code uses class Reader and Writer.
 
 
 public class MainMethod {
   public static void main (String argv[]) {
+    boolean IObound = false;
+    boolean CPUbound = true;
 
     // Create a shared data structure.  This simulation only uses an integer
     // as the "shared data".  The value of this integer is used in the output,
@@ -21,14 +18,17 @@ public class MainMethod {
     // to access and update a more complex data structure.
     SharedDataStruct sharedData;         // declare sharedData
     sharedData = new SharedDataStruct(); // assign a value to sharedData
-    
+
     System.out.println("The simiulation of the computer system is starting"); // initial print of the computer system
 
     UserJob U;
+    UserJob V;
 
     for (int i=1; i <=4; i++) {
-      U = new UserJob(i, sharedData);
+      U = new UserJob(i, sharedData, IObound);
       U.start();
+      V = new UserJob(i, sharedData, CPUbound);
+      V.start();
     }
 
   }  // end of "main"
--- UserJob.java
+++ UserJob.java
@@ -1,29 +1,43 @@
 /**
  * Created by eccrawford on 2018-03-26.
  */
-public class UserJob extends Thread {
+public class UserJob extends Thread{
     int myName;
     SharedDataStruct sharedData;
+    boolean bound;
 
 
-    public UserJob(int name, SharedDataStruct SD) {
+    public UserJob(int name, SharedDataStruct SD, boolean destination) {
         myName = name;    // copy the parameter value to local variable "MyName"
         sharedData = SD;  // sharedData refers to the SharedDataStruct object
+        bound = destination;
     }
 
     public void run () {
-        int val;  // The value read from the database
-        int CPUtime = 100;
-        int IOtime = 300;
+        int CPUtime = 100 + (int)(Math.random() * ((1000-100)+1));
+        int IOtime = 1 + (int)(Math.random() * ((200-1)+1));
 
         for (int I = 0;  I < 5; I++) {
             System.out.println("UserJob "+I+ " is starting");
 
             // Simulate "doing something else" by delaying for a while.
-            System.out.println( "UserJob "+ I+ " starting CPU burst of length "+ CPUtime);
-            try { sleep(CPUtime); } catch(Exception e) {break;}
-            System.out.println("UserJob " + I + " starting IO burst of length " + IOtime);
-            try { sleep( IOtime); } catch(Exception e) {break;}
+            if (bound) {
+                System.out.println("UserJob " + I + " starting CPU burst of length " + CPUtime);
+
+                try {
+                    sleep(CPUtime);
+                } catch (Exception e) {
+                    break;
+                } // shorter time
+            }
+            else {
+                System.out.println("UserJob " + I + " starting IO burst of length " + IOtime);
+                try {
+                    sleep(IOtime);
+                    } catch (Exception e) {
+                        break;
+                    } // longer time
+                }
         } // end of "for" loop
 
     }  // end of "run" method
